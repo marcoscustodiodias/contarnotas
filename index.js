44fafa1aa70c56707dae4bb928493f2a1405bb7c
Marcos Dias <marcosdias.pvai@gmail.com>
2021-11-15 09:25:25 -0300

    feat: cria a estrutura inicial do projeto com a função de contar notas

---
+++ index.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+let n = parseInt(prompt());
+let quantidadeNotas = 0;
+let cedulas = [100, 50, 20, 10, 5, 2, 1];
+
+// Função responsável por contar as notas a partir de um valor.
+function contaNotas(valor) {
+  quantidadeNotas = parseInt(n / valor);
+
+  n -= quantidadeNotas * valor;
+
+  console.log(`${quantidadeNotas} nota(s) de R$ ${valor},00`);
+}
+
+console.log(n);
+
+for (let cedula in cedulas) {
+  contaNotas(cedulas[cedula]);
+}
